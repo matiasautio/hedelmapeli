ea9d90fd62fc41c04817e4c5d5efa4573599f4c3
taturl <taturl@github.com>
2021-09-02 22:25:31 +0300

    Add prizetable json and winchecking

--- components/init.js
+++ components/init.js
@@ -2,3 +2,125 @@ let coinSound = document.getElementById("coinSound");
 let canDisplaySplash = true;
 
 var slots_0 = ["null", "🍓", "🍒", "🍇", "🍊", "🍋", "🍍"];
+
+const PRIZETABLE = [
+  {
+    prize: 3,
+    line: ["🍋", "🍋", "🍋"],
+  },
+
+  {
+    prize: 2,
+    line: ["🍋", NaN, "🍋"],
+  },
+
+  {
+    prize: 1,
+    line: ["🍋", "🍋", NaN],
+  },
+
+  {
+    prize: 1,
+    line: [NaN, "🍋", "🍋"],
+  },
+
+  {
+    prize: 3,
+    line: ["🍓", "🍓", "🍓"],
+  },
+
+  {
+    prize: 2,
+    line: ["🍓", NaN, "🍓"],
+  },
+
+  {
+    prize: 1,
+    line: ["🍓", "🍓", NaN],
+  },
+
+  {
+    prize: 1,
+    line: [NaN, "🍓", "🍓"],
+  },
+
+  {
+    prize: 3,
+    line: ["🍒", "🍒", "🍒"],
+  },
+
+  {
+    prize: 2,
+    line: ["🍒", NaN, "🍒"],
+  },
+
+  {
+    prize: 1,
+    line: ["🍒", "🍒", NaN],
+  },
+
+  {
+    prize: 1,
+    line: [NaN, "🍒", "🍒"],
+  },
+
+  {
+    prize: 3,
+    line: ["🍇", "🍇", "🍇"],
+  },
+
+  {
+    prize: 2,
+    line: ["🍇", NaN, "🍇"],
+  },
+
+  {
+    prize: 1,
+    line: ["🍇", "🍇", NaN],
+  },
+
+  {
+    prize: 1,
+    line: [NaN, "🍇", "🍇"],
+  },
+
+  {
+    prize: 3,
+    line: ["🍊", "🍊", "🍊"],
+  },
+
+  {
+    prize: 2,
+    line: ["🍊", NaN, "🍊"],
+  },
+
+  {
+    prize: 1,
+    line: ["🍊", "🍊", NaN],
+  },
+
+  {
+    prize: 1,
+    line: [NaN, "🍊", "🍊"],
+  },
+
+  {
+    prize: 3,
+    line: ["🍍", "🍍", "🍍"],
+  },
+
+  {
+    prize: 2,
+    line: ["🍍", NaN, "🍍"],
+  },
+
+  {
+    prize: 1,
+    line: ["🍍", "🍍", NaN],
+  },
+
+  {
+    prize: 1,
+    line: [NaN, "🍍", "🍍"],
+  },
+];
