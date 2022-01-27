41a2e615f4c2136c2555da6036b05e4f780acd6c
taturl <taturl@github.com>
2022-01-27 20:28:47 +0200

    Move scripts to own directory

--- README.md
+++ README.md
@@ -8,4 +8,11 @@ Documentation here: https://aframe.io/docs/1.2.0/introduction/
 Press CTRL + ALT + I in browser to open A-Frame's own internal scene editor.
 Nifty tool for model viewing: https://modelviewer.dev/editor/
 
+## How to run locally
+
+```
+npm install
+npm start
+```
+
 © A _two tatus_ construction
---
+++ components2/game.js
@@ -0,0 +1,29 @@
+let canRoll = true;
+
+function lastReelEventListener(){
+    const lastReel = document.getElementById("reel2");
+    lastReel.addEventListener('animationcomplete', function(){
+        canRoll = true;
+    });
+  };
+
+const element = document.getElementById("playBtn");
+element.addEventListener("click", function() {
+  if(canRoll) {
+    canRoll = false;
+    let reels = [];
+    reels.push(document.getElementById("reel0"));
+    reels.push(document.getElementById("reel1"));
+    reels.push(document.getElementById("reel2"));
+    let delay = 0;
+    //document.getElementById("reel0").setAttribute("animation", "property: rotation; to: 360 0 0; dur: 2000; easing: linear; loop: false")
+    reels.forEach(element => {
+      console.log("Reels rolling");
+      // tried delay for each reel, doesn't work yet
+      // for what ever reason the animation needs to be reset first
+      element.setAttribute("animation", "property: rotation; from: 0 0 0; to: 0 0 0");
+      element.setAttribute("animation", "property: rotation; from: 0 0 0; to: 360 0 0; delay: "+delay+"");
+      delay += 200;
+    });
+  }
+});
