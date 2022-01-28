df925e4b0b4a6e2e5872a2b7a38bc6efb8e0e983
tatsoni_desktop_1 <tatu.heinamaki@gmail.com>
2022-01-28 14:16:22 +0200

    Fixed problem with reel object's children having arbitary order.

--- components2/game.js
+++ components2/game.js
@@ -1,11 +1,23 @@
 let canRoll = true;
 
-function lastReelEventListener(){
-    const lastReel = document.getElementById("reel2");
-    lastReel.addEventListener('animationcomplete', function(){
-        canRoll = true;
+AFRAME.registerComponent('reorderobjectcchildren', {
+  init: function(){
+    this.el.addEventListener('model-loaded', () => {
+      const obj = this.el.getObject3D('mesh');
+      var children = Array.from(obj.children);
+      children.sort((a, b) => a.name.localeCompare(b.name));
+      console.log(children);
+      obj.children = children;
     });
-  };
+  }
+});
+
+function lastReelEventListener(){
+  const lastReel = document.getElementById("reel2");
+  lastReel.addEventListener('animationcomplete', function(){
+    canRoll = true;
+  });
+};
 
 const element = document.getElementById("playBtn");
 element.addEventListener("click", function() {
@@ -19,12 +31,12 @@ element.addEventListener("click", function() {
     //document.getElementById("reel0").setAttribute("animation", "property: rotation; to: 360 0 0; dur: 2000; easing: linear; loop: false")
     reels.forEach(element => {
       console.log("Reels rolling");
-      // tried delay for each reel, doesn't work yet
       // for what ever reason the animation needs to be reset first
       element.setAttribute("animation", "property: rotation; from: 0 0 0; to: 0 0 0");
       element.setAttribute("animation", "property: rotation; from: 0 0 0; to: 360 0 0; delay: "+delay+"");
+      element.getObject3D('mesh').children[9].material.color = new THREE.Color( 'skyblue' );
+      console.log(element.getObject3D('mesh').children);
       delay += 200;
-      element.getObject3D('mesh').children[0].material.color = new THREE.Color( 'skyblue' );
     });
   }
 });
