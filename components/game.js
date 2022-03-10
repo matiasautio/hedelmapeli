08d595f0da14ecde8d0d9c75446b5ff603c7a92e
taturl <taturl@github.com>
2022-03-10 21:45:31 +0200

    Add TJQK icons

--- components/game.js
+++ components/game.js
@@ -1,5 +1,12 @@
 let canRoll = true;
 
+let texture_pool = [
+  new THREE.TextureLoader().load( "2D_assets/T.png"),
+  new THREE.TextureLoader().load( "2D_assets/J.png"),
+  new THREE.TextureLoader().load( "2D_assets/Q.png"),
+  new THREE.TextureLoader().load( "2D_assets/K.png")
+];
+
 AFRAME.registerComponent('reorderobjectcchildren', {
   init: function(){
     this.el.addEventListener('model-loaded', () => {
@@ -19,6 +26,18 @@ function lastReelEventListener(){
   });
 };
 
+function GetRotationTargetFromCurrentPosition(current_rotation, angle_to_change){
+  const rotation_target = THREE.Math.radToDeg(current_rotation) + angle_to_change;
+  return(rotation_target);
+};
+
+function GetUpcomingFaceFromCurrentPosition(quaternion, angle_to_change, sides){
+  const euler = new THREE.Euler( 0, 0, 0 );
+  euler.setFromQuaternion( quaternion );
+  const face = (Math.round(THREE.Math.radToDeg(euler.x)) + angle_to_change) / (360 / sides);
+  return(face);
+};
+
 const element = document.getElementById("playBtn");
 element.addEventListener("click", function() {
   if(canRoll) {
@@ -28,17 +47,27 @@ element.addEventListener("click", function() {
     reels.push(document.getElementById("reel1"));
     reels.push(document.getElementById("reel2"));
     let delay = 0;
-    //document.getElementById("reel0").setAttribute("animation", "property: rotation; to: 360 0 0; dur: 2000; easing: linear; loop: false")
+
     reels.forEach(element => {
       console.log("Reels rolling");
-      // console.log(element);
-      let rotation_target = THREE.Math.radToDeg(element.object3D.rotation.x + Math.PI);
+
+      // Get indices of current hidden-from-view slots
+      // Change those slots to random slots
+
+      const angle_to_change = 30;
+      const sides = 12;
+      const rotation_target = GetRotationTargetFromCurrentPosition(element.object3D.rotation.x, angle_to_change);
+      const face = GetUpcomingFaceFromCurrentPosition(element.object3D.quaternion, angle_to_change, sides);
+
       element.setAttribute("animation", "property: rotation; to: "+rotation_target+" 0 0; delay: "+delay+"");
       // element.getObject3D('mesh').children[11].material.color = new THREE.Color( 'skyblue' );
       // console.log(element.getObject3D('mesh').children);
       delay += 200;
     });
-    reels[0].getObject3D('mesh').children[0].material.color = new THREE.Color( 'skyblue' );
-    reels[0].getObject3D('mesh').children[0].material.map = new THREE.TextureLoader().load( "2D_assets/q_paint_inv.png");
+
+    reels[2].getObject3D('mesh').children.forEach(slot => {
+      slot.material.map = texture_pool[1];
+    });
+
   }
 });
