0882db85d65d8b5a2567e1bc257b2ef8f63a69ab
taturl <taturl@github.com>
2022-03-11 19:20:25 +0200

    Add formatting json

--- components/game.js
+++ components/game.js
@@ -1,95 +1,123 @@
 let canRoll = true;
 
-let texture_pool = [
-  new THREE.TextureLoader().load( "2D_assets/T.png"),
-  new THREE.TextureLoader().load( "2D_assets/J.png"),
-  new THREE.TextureLoader().load( "2D_assets/Q.png"),
-  new THREE.TextureLoader().load( "2D_assets/K.png")
-];
+let texturePool = {
+  T: new THREE.TextureLoader().load('2D_assets/T.png'),
+  J: new THREE.TextureLoader().load('2D_assets/J.png'),
+  Q: new THREE.TextureLoader().load('2D_assets/Q.png'),
+  K: new THREE.TextureLoader().load('2D_assets/K.png'),
+};
+
+function RandomPropertyFrom(obj) {
+  let keys = Object.keys(obj);
+  let randomProperty = obj[keys[(keys.length * Math.random()) << 0]];
+  return randomProperty;
+}
 
 AFRAME.registerComponent('reorderobjectcchildren', {
-  init: function(){
+  init: function () {
     this.el.addEventListener('model-loaded', () => {
+      // Reorder objects children
       const obj = this.el.getObject3D('mesh');
       var children = Array.from(obj.children);
       children.sort((a, b) => a.name.localeCompare(b.name));
-      console.log(children);
       obj.children = children;
 
-      obj.children.forEach(face => {
-        face.material.color = new THREE.Color( 'skyblue' );
-        face.material.map = texture_pool[THREE.Math.randInt(0, texture_pool.length - 1)];
+      // Add initial textures & color
+      obj.children.forEach((face) => {
+        face.material.color = new THREE.Color('skyblue');
+        face.material.map = RandomPropertyFrom(texturePool);
       });
     });
-  }
+  },
 });
 
-function lastReelEventListener(){
-  const lastReel = document.getElementById("reel2");
-  lastReel.addEventListener('animationcomplete', function(){
+function lastReelEventListener() {
+  const lastReel = document.getElementById('reel2');
+  lastReel.addEventListener('animationcomplete', function () {
     canRoll = true;
   });
-};
+}
 
-function GetRotationTargetFromCurrentPosition(current_rotation, angle_to_change){
-  const rotation_target = THREE.Math.radToDeg(current_rotation) + angle_to_change;
-  return(rotation_target);
-};
+function GetRotationTargetFromCurrentPosition(
+  current_rotation,
+  angle_to_change
+) {
+  const rotation_target =
+    THREE.Math.radToDeg(current_rotation) + angle_to_change;
+  return rotation_target;
+}
 
-function GetFaceFromPosition(quaternion, angle_to_change, sides){
-  const euler = new THREE.Euler( 0, 0, 0 );
-  euler.setFromQuaternion( quaternion );
+function GetFaceFromPosition(quaternion, angle_to_change, sides) {
+  const euler = new THREE.Euler(0, 0, 0);
+  euler.setFromQuaternion(quaternion);
   let degree = Math.round(THREE.Math.radToDeg(euler.x));
 
-  if(degree < 0) {
+  if (degree < 0) {
     degree += 360;
   }
 
   const face = (degree + angle_to_change) / (360 / sides);
-  return(face);
-};
+  return face;
+}
 
-function GetHiddenFacesFromOppositeFace(face){
-  const default_hidden_faces = [
-    3, 4, 5, 6, 7, 8, 9
-  ];
+function GetHiddenFacesFromOppositeFace(face) {
+  const default_hidden_faces = [3, 4, 5, 6, 7, 8, 9];
 
-  const current_hidden_faces = default_hidden_faces.map( function(value) {
+  const current_hidden_faces = default_hidden_faces.map(function (value) {
     return (value + face) % 12;
-  } );
+  });
 
-  return(current_hidden_faces);
-};
+  return current_hidden_faces;
+}
 
-const element = document.getElementById("playBtn");
-element.addEventListener("click", function() {
-  if(canRoll) {
+const element = document.getElementById('playBtn');
+element.addEventListener('click', function () {
+  if (canRoll) {
     canRoll = false;
     let reels = [];
-    reels.push(document.getElementById("reel0"));
-    reels.push(document.getElementById("reel1"));
-    reels.push(document.getElementById("reel2"));
+    reels.push(document.getElementById('reel0'));
+    reels.push(document.getElementById('reel1'));
+    reels.push(document.getElementById('reel2'));
     let delay = 0;
 
-    reels.forEach(element => {
-      console.log("Reels rolling");
+    reels.forEach((element) => {
+      console.log('Reels rolling');
 
       // Get indices of current hidden-from-view slots
       // Change those slots to random slots
 
       const angle_to_change = 180;
       const sides = 12;
-      const rotation_target = GetRotationTargetFromCurrentPosition(element.object3D.rotation.x, angle_to_change);
+      const rotation_target = GetRotationTargetFromCurrentPosition(
+        element.object3D.rotation.x,
+        angle_to_change
+      );
 
-      const current_face = GetFaceFromPosition(element.object3D.quaternion, 0, sides);
-      const next_face = GetFaceFromPosition(element.object3D.quaternion, angle_to_change, sides);
+      const current_face = GetFaceFromPosition(
+        element.object3D.quaternion,
+        0,
+        sides
+      );
+      const next_face = GetFaceFromPosition(
+        element.object3D.quaternion,
+        angle_to_change,
+        sides
+      );
       const current_hidden_faces = GetHiddenFacesFromOppositeFace(current_face);
 
       for (const face of current_hidden_faces) {
-        element.getObject3D('mesh').children[face].material.map = texture_pool[THREE.Math.randInt(0, texture_pool.length - 1)];
+        element.getObject3D('mesh').children[face].material.map =
+          RandomPropertyFrom(texturePool);
       }
 
-      element.setAttribute("animation", "property: rotation; to: "+rotation_target+" 0 0; delay: "+delay+"");
+      element.setAttribute(
+        'animation',
+        'property: rotation; to: ' +
+          rotation_target +
+          ' 0 0; delay: ' +
+          delay +
+          ''
+      );
 
       delay += 200;
     });
